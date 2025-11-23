e8c9d0557f864083b71404f8adcdc37633752071
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-11-23 19:28:16 -0300

    Get posts query

--- src/app/ReactQueryProvider.jsx
+++ src/app/ReactQueryProvider.jsx
@@ -5,5 +5,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 export const ReactQueryProvider = ({ children }) => {
-  return <QueryClientProvider>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,32 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { CardPost } from "@/components/CardPost";
 import { Spinner } from "@/components/Spinner";
 import styles from "./page.module.css";
 import Link from "next/link";
 
+const fetchPosts = async ({ page }) => {
+  const results = await fetch(`http://localhost:3000/api/posts?page=${page}`);
+
+  const data = await results.json();
+
+  return data;
+};
+
 export default function Home({ searchParams }) {
   const currentPage = parseInt(searchParams?.page || 1);
   const searchTerm = searchParams?.q;
 
-  const isLoading = false;
-  const isFetching = false;
-  const posts = [];
+  const { data: posts } = useQuery({
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts({ page: currentPage }),
+  });
 
   const ratingsAndCartegoriesMap = null;
+
+  const isLoading = false;
+  const isFetching = false;
 
   return (
     <main className={styles.grid}>
